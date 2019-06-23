f611d70b7447d8fdbbde9c8c5bedaef940be5b7b
Anik Das <anikdas012@gmail.com>
2019-06-23 14:36:25 +0600

    Saving table name in an ArrayList

--- src/main/java/com/datasoft/javaengineersassessment/solution/Solution.java
+++ src/main/java/com/datasoft/javaengineersassessment/solution/Solution.java
@@ -2,6 +2,8 @@ package com.datasoft.javaengineersassessment.solution;
 
 import com.datasoft.javaengineersassessment.utils.IO;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Objects;
 
 public class Solution implements Runnable{
@@ -29,6 +31,11 @@ public class Solution implements Runnable{
 			String[] temp = IO.readLine().split(" ");
 			int numberOfTables = Integer.parseInt(temp[0]);
 			int numberOfJsonData = Integer.parseInt(temp[1]);
+			temp = IO.readLine().split(" ");
+			ArrayList<String> tableNames = new ArrayList<>();
+			if (numberOfTables == temp.length) {
+				tableNames.addAll(Arrays.asList(temp));
+			}
 		}
 	}
 }
