5a10743c47dbcdc5d43a4a99463a9cf5252533fd
Anik Das <anikdas012@gmail.com>
2019-06-23 14:41:54 +0600

    Parsing and storing Json Objects into an ArrayList

--- src/main/java/com/datasoft/javaengineersassessment/solution/Solution.java
+++ src/main/java/com/datasoft/javaengineersassessment/solution/Solution.java
@@ -1,6 +1,8 @@
 package com.datasoft.javaengineersassessment.solution;
 
 import com.datasoft.javaengineersassessment.utils.IO;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -35,6 +37,28 @@ public class Solution implements Runnable{
 			ArrayList<String> tableNames = new ArrayList<>();
 			if (numberOfTables == temp.length) {
 				tableNames.addAll(Arrays.asList(temp));
+			}
+			ArrayList<JsonObject> jsonObjects = new ArrayList<>();
+			for (int j = 0; j < numberOfJsonData; j++) {
+				StringBuilder jobj = new StringBuilder();
+				int startingBrace = 0;
+				int endingBrace = 0;
+				while (true) {
+					String z = IO.readLine();
+					if (z != null) {
+						if (z.equals("{")) {
+							startingBrace++;
+						}
+						if (z.equals("}")) {
+							endingBrace++;
+						}
+						jobj.append(z);
+						if (startingBrace == endingBrace) {
+							break;
+						}
+					}
+				}
+				jsonObjects.add(new JsonParser().parse(jobj.toString()).getAsJsonObject());
 			}
 		}
 	}
